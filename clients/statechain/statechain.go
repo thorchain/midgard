e9eab10246f4cf9d77876c4216d82bf5daa2fb95
Son of Odin <sonofodin-thorchain@protonmail.com>
2019-08-28 18:32:35 +0200

    support swap fees

--- clients/statechain/statechain.go
+++ clients/statechain/statechain.go
@@ -143,7 +143,11 @@ func (sc *StatechainAPI) GetPoints(id int64) (int64, []client.Point, error) {
 				int64(evt.ID.Float64()),
 				rAmt,
 				tAmt,
-				swap.Slip.Float64(),
+				swap.PriceSlip.Float64(),
+				swap.TradeSlip.Float64(),
+				swap.PoolSlip.Float64(),
+				swap.OutputSlip.Float64(),
+				swap.Fee.Float64(),
 				evt.Pool,
 				tx.Timestamp,
 			).Point())
--- store/influxdb/swap_test.go
+++ store/influxdb/swap_test.go
@@ -1,6 +1,7 @@
 package influxdb
 
 import (
+	"fmt"
 	"time"
 
 	"gitlab.com/thorchain/bepswap/common"
@@ -18,14 +19,23 @@ func (s *SwapEventSuite) TestSwapEvent(c *C) {
 		1,
 		12.3,
 		14.4,
-		0.07,
+		0.1,
+		0.2,
+		0.3,
+		0.4,
+		0.5,
 		common.Ticker("BNB"),
 		time.Now(),
 	)
 
 	c.Check(swap.RuneAmount, Equals, 12.3)
 	c.Check(swap.TokenAmount, Equals, 14.4)
-	c.Check(swap.Slip, Equals, 0.07)
+	c.Check(swap.PriceSlip, Equals, 0.1)
+	c.Check(swap.TradeSlip, Equals, 0.2)
+	c.Check(swap.PoolSlip, Equals, 0.3)
+	c.Check(swap.OutputSlip, Equals, 0.4)
+	c.Check(swap.RuneFee, Equals, 0.5)
+	c.Check(swap.TokenFee, Equals, 0.0)
 	c.Check(swap.Pool.String(), Equals, "BNB")
 
 	err := clc.AddEvent(swap)
@@ -37,5 +47,9 @@ func (s *SwapEventSuite) TestSwapEvent(c *C) {
 	c.Assert(resp, HasLen, 1)
 	c.Assert(resp[0].Series, HasLen, 1)
 	c.Assert(resp[0].Series[0].Values, HasLen, 1)
-	c.Check(resp[0].Series[0].Values[0][2], Equals, "BNB", Commentf("%+v", resp[0].Series[0].Values))
+
+	fmt.Printf("SERIES: %+v\n", resp[0].Series[0])
+	v, ok := getStringValue(resp[0].Series[0], "pool")
+	c.Check(ok, Equals, true)
+	c.Check(v, Equals, "BNB", Commentf("%+v", resp[0].Series[0].Values))
 }
