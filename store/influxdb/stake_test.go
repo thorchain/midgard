714641e7205130f7bba0964781239f57ba5609ab
Chad Barraford <cbarraford@gmail.com>
2019-08-28 21:47:44 +0200

    add staker influxdb queries

--- store/influxdb/stake_test.go
+++ store/influxdb/stake_test.go
@@ -13,15 +13,18 @@ var _ = Suite(&StakeEventSuite{})
 
 func (s *StakeEventSuite) TestStakeEvent(c *C) {
 	clc := NewTestClient(c)
+	ticker := common.Ticker("BNB")
+	addr := common.BnbAddress("bnbblejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6")
+	now := time.Now()
 
 	stake := NewStakeEvent(
 		1,
 		12.3,
 		14.4,
 		5.1,
-		common.Ticker("BNB"),
-		common.BnbAddress("bnbblejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6"),
-		time.Now(),
+		ticker,
+		addr,
+		now,
 	)
 
 	c.Check(stake.RuneAmount, Equals, 12.3)
@@ -46,4 +49,18 @@ func (s *StakeEventSuite) TestStakeEvent(c *C) {
 			c.Check(resp[0].Series[0].Values[0][i], Equals, "BNB", Commentf("%+v", resp[0].Series[0].Values))
 		}
 	}
+
+	tickers, err := clc.ListStakerPools(common.BnbAddress("bnbblejrrtta9cgr49fuh7ktu3sddhe0ff7wenlpn6"))
+	c.Assert(err, IsNil)
+	c.Assert(tickers, HasLen, 1)
+	c.Check(tickers[0].String(), Equals, "BNB")
+
+	staker, err := clc.GetStakerDataForPool(ticker, addr)
+	c.Assert(err, IsNil)
+	c.Check(staker.Ticker.Equals(ticker), Equals, true)
+	c.Check(staker.Address.Equals(addr), Equals, true)
+	c.Check(staker.Rune, Equals, 12.3)
+	c.Check(staker.Token, Equals, 14.4)
+	c.Check(staker.Units, Equals, 5.1)
+	c.Check(staker.DateFirstStaked.UnixNano(), Equals, now.UnixNano())
 }
