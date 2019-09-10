15dcec330a81e331529dd0de3427f880d6ff4e7e
Buri <buri@thorchain.org>
2019-09-10 08:50:06 +0800

    Hardcode the CORS origin domain.

--- server.go
+++ server.go
@@ -82,7 +82,7 @@ func NewServer(cfg config.Configuration) (*Server, error) {
 
 func CORS() gin.HandlerFunc {
   return func(c *gin.Context) {
-    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+    c.Writer.Header().Set("Access-Control-Allow-Origin", "https://testnet.bepswap.com")
     c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
     c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
     c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
