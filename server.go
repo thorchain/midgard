0d670206e2f1e450cd0bea8c3470c31d19afc559
Buri <buri@thorchain.org>
2019-09-10 09:28:13 +0800

    Programmatically get the origin.

--- server.go
+++ server.go
@@ -82,7 +82,8 @@ func NewServer(cfg config.Configuration) (*Server, error) {
 
 func CORS() gin.HandlerFunc {
   return func(c *gin.Context) {
-    c.Writer.Header().Set("Access-Control-Allow-Origin", "https://testnet.bepswap.com")
+		origin := c.Request.Header.Get("Origin")
+    c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
     c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
     c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
     c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
