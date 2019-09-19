9863a06589d2280ecfdb8978046f9e08e80eb07c
Igor <khigor777@yandex.ru>
2019-09-19 14:57:28 +0300

    fix test

--- clients/coingecko/price_test.go
+++ clients/coingecko/price_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestNewPriceService(t *testing.T) {
 	ps := NewPriceService(NewCache(), "thorchain", "usd")
-	go ps.Run(time.Second * 3)
+	ch := make(chan struct{}, 1)
+	go ps.Run(time.Second*3, ch)
 	time.Sleep(time.Second * 4)
 	resp, err := ps.GetPrice()
 	if err != nil {
@@ -16,4 +17,5 @@ func TestNewPriceService(t *testing.T) {
 	if resp.CoinName == "" || resp.Price == 0 || resp.CurrencyName == "" {
 		t.Error("empty data")
 	}
+	close(ch)
 }
