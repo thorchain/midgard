1f02040583c518b1badf9fce1c5d379a4f01034d
Luke Hamilton <luke@mettadata.com>
2019-10-07 20:03:20 +1100

    Fixed broken placeholder test.

--- api/rest/v1/handlers/handlers_test.go
+++ api/rest/v1/handlers/handlers_test.go
@@ -2,6 +2,9 @@ package handlers
 
 import (
 	api "gitlab.com/thorchain/bepswap/chain-service/api/rest/v1/codegen"
+	"gitlab.com/thorchain/bepswap/chain-service/config"
+
+	"gitlab.com/thorchain/bepswap/chain-service/store/influxdb"
 
 	"testing"
 
@@ -11,7 +14,8 @@ import (
 func setupService(t *testing.T) *echo.Echo {
 	var err error
 	e := echo.New()
-	s := New()
+	store, _ := influxdb.NewClient(config.InfluxDBConfiguration{})
+	s := New(store)
 
 	swagger, err := api.GetSwagger()
 	if err != nil {
