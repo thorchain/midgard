3a68e97e5542946c881f1e08e466e37b8362429c
Loki Laufeyson <l031@protonmail.com>
2019-10-30 14:45:03 +1100

    WIP: * schema updates * prototyping a better layout

---
+++ internal/clients/binance/binance.go
@@ -0,0 +1,7 @@
+package binance
+
+import "gitlab.com/thorchain/bepswap/chain-service/internal/clients"
+
+type Binance struct {
+	clients.Base
+}
---
+++ internal/clients/coinmarketcap/coinmarketcap.go
@@ -0,0 +1,7 @@
+package coinmarketcap
+
+import "gitlab.com/thorchain/bepswap/chain-service/internal/clients"
+
+type Coinmarketcap struct {
+	clients.Base
+}
