50ecbed0fe2f98cdef7b01075b29dd1e128258c2
Loki Laufeyson <l031@protonmail.com>
2019-11-04 15:29:04 +1100

    * deleted RnD code.

--- internal/clients/binance/binance.go
+++
@@ -1,7 +0,0 @@
-package binance
-
-import "gitlab.com/thorchain/bepswap/chain-service/internal/clients"
-
-type Binance struct {
-	clients.Base
-}
--- internal/clients/coingecko/coingecko.go
+++
@@ -1,7 +0,0 @@
-package coingecko
-
-import "gitlab.com/thorchain/bepswap/chain-service/internal/clients"
-
-type Coingecko struct {
-	clients.Base
-}
