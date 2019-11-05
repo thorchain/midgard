5ea8e721f48e51025603df554db45cddc2b91858
Loki Laufeyson <l031@protonmail.com>
2019-11-05 18:30:49 +1100

    * Updated skate event to support new Asset format * Updated MockServer with an /pool/{asset} endpoint * Added getAllTokens, ensureTokensDataAvailable, getTokens, for the GetToken to return the token data from the binance api * Updated /v1/assets/{asset} to populate the Name field

--- api/rest/v1/handlers/handlers.go
+++ api/rest/v1/handlers/handlers.go
@@ -105,12 +105,17 @@ func (h *Handlers) GetAssetInfo(ctx echo.Context, asset string) error {
 	}
 
 	t := time.Now()
+	n, err := h.binanceClient.GetToken(pool.Asset)
+	if err != nil {
+		h.logger.Error().Err(err).Msg("fail to get token data from binance")
+		return echo.NewHTTPError(http.StatusBadRequest, api.GeneralErrorResponse{Error: "fail to get token data from binance"})
+	}
 
 	res := api.AssetsDetailedResponse{
 		Asset:       helpers.ConvertAssetForAPI(pool.Asset),
 		DateCreated: &t,
 		Logo:        pointy.String(fmt.Sprintf("%s://%s/blockchains/binance/assets/bnb/logo.png", ctx.Scheme(), ctx.Request().Host)),
-		Name:        pointy.String("COIN_NAME"),
+		Name:        pointy.String(n.Name),
 		PriceRune:   pointy.Float64(1.0),
 		PriceUSD:    pointy.Float64(2.0),
 	}
