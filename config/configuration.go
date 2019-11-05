36e788c29137817bcb7173639cbab533dd15cb28
Luke Hamilton <luke@mettadata.com>
2019-11-05 18:30:49 +1100

    * Updated skate event to support new Asset format * Updated MockServer with an /pool/{asset} endpoint * Added getAllTokens, ensureTokensDataAvailable, getTokens, for the GetToken to return the token data from the binance api * Updated /v1/assets/{asset} to populate the Name field

--- config/configuration.go
+++ config/configuration.go
@@ -46,6 +46,7 @@ type BinanceConfiguration struct {
 	Scheme               string        `json:"scheme" mapstructure:"scheme"`
 	RequestTimeout       time.Duration `json:"request_timeout" mapstructure:"request_timeout"`
 	MarketsCacheDuration time.Duration `json:"markets_cache_duration" mapstructure:"markets_cache_duration"`
+	TokensCacheDuration  time.Duration `json:"tokens_cache_duration" mapstructure:"tokens_cache_duration"`
 	FullNodeHost         string        `json:"full_node_host" mapstructure:"full_node_host"`
 	FullNodeScheme       string        `json:"full_node_scheme" mapstructure:"full_node_scheme"`
 	IsTestNet            bool          `json:"is_testnet" mapstructure:"is_testnet"`
