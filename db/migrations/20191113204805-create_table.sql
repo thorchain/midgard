8a4d5ddee630c21fcf5dd87b447ad9ec4844b136
Luke Hamilton <luke@mettadata.com>
2019-11-14 15:53:37 +1100

    WIP: new schema

--- db/migrations/20191113204805-create_table.sql
+++ db/migrations/20191113204805-create_table.sql
@@ -25,55 +25,60 @@ CREATE TABLE events (
 
 CREATE TABLE stakes (
     time        TIMESTAMPTZ       NOT NULL,
-    event_id integer references events(id),
+    event_id integer not null ,
     chain integer not null,
     symbol varchar not null,
     ticker varchar not null,
     units integer,
     primary key (time, event_id)
 );
+
 --
--- CREATE TYPE tx_direction as enum('in', 'out');
--- CREATE TABLE txs (
---     time        TIMESTAMPTZ       NOT NULL,
---     hash varchar not null,
---     event_id integer references events(id),
---     direction tx_direction not null,
---     chain varchar,
---     from_address varchar,
---     to_address varchar,
---     memo varchar,
---     primary key (time, event_id)
--- );
---
--- CREATE TABLE coins (
---     time        TIMESTAMPTZ       NOT NULL,
---     tx_id integer references txs(id),
---     event_id integer references events(id),
---     chain varchar not null,
---     symbol varchar not null,
---     ticker varchar not null,
---     amount integer not null,
---     primary key (time, tx_id, event_id)
--- );
---
--- CREATE TABLE gas_coins (
---     time        TIMESTAMPTZ       NOT NULL,
---     event_id integer references events(id),
---     chain varchar not null,
---     symbol varchar not null,
---     ticker varchar not null,
---     amount integer not null,
---     primary key (time, event_id)
--- );
+CREATE TYPE tx_direction as enum('in', 'out');
+CREATE TABLE txs (
+    time        TIMESTAMPTZ       NOT NULL,
+    hash varchar not null,
+    event_id integer not null,
+    direction tx_direction not null,
+    chain varchar,
+    from_address varchar,
+    to_address varchar,
+    memo varchar,
+    primary key (time, event_id)
+);
+
+CREATE TABLE coins (
+    time        TIMESTAMPTZ       NOT NULL,
+    tx_id integer not null,
+    event_id integer not null,
+    chain varchar not null,
+    symbol varchar not null,
+    ticker varchar not null,
+    amount integer not null,
+    primary key (time, tx_id, event_id)
+);
+
+CREATE TABLE gas_coins (
+    time        TIMESTAMPTZ       NOT NULL,
+    event_id integer not null,
+    chain varchar not null,
+    symbol varchar not null,
+    ticker varchar not null,
+    amount integer not null,
+    primary key (time, event_id)
+);
 
 SELECT create_hypertable('events', 'time');
 SELECT create_hypertable('stakes', 'time');
--- SELECT create_hypertable('txs', 'time', 'event_id');
--- SELECT create_hypertable('coins', 'time', 'tx_id', 'event_id');
--- SELECT create_hypertable('gas_coins', 'time', 'event_id');
+SELECT create_hypertable('txs', 'time');
+SELECT create_hypertable('coins', 'time');
+SELECT create_hypertable('gas_coins', 'time');
 
 -- +migrate Down
 
 DROP TABLE events;
-
+DROP TABLE stakes;
+DROP TABLE txs;
+DROP TABLE coins;
+DROP TABLE gas_coins;
+DROP TYPE tx_direction;
