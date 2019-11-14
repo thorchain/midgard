d17287e90f8a58e868a9b82cfadb844041827aa6
Brent McDowell <brent@bmcd.co>
2019-11-14 14:59:50 +0800

    Linter and Gas fixes

--- internal/common/gas.go
+++ internal/common/gas.go
@@ -2,22 +2,8 @@ package common
 
 type Gas []GasItem
 type GasItem struct {
-	EventID int64  `json:"event_id"`
 	Chain   Chain  `json:"chain"`
 	Symbol  string `json:"symbol"`
 	Ticker  string `json:"ticker"`
 	Amount  int64  `json:"amount"`
-}
-
-func NewGas(eventId int64, chain Chain, symbol, ticker string, amount int64) Gas {
-	var gas Gas
-	gas = append(gas, GasItem{
-		eventId,
-		chain,
-		symbol,
-		ticker,
-		amount},
-	)
-
-	return gas
 }
