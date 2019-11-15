dad02a97a76e30dbe480d208ff0d6503e1c6f7cc
Loki Laufeyson <l031@protonmail.com>
2019-11-15 15:38:46 +1100

    * Updated stakes.GetStakerAddresses() method.

--- internal/store/timescale/stakes.go
+++ internal/store/timescale/stakes.go
@@ -2,14 +2,19 @@ package timescale
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 
+	"gitlab.com/thorchain/bepswap/chain-service/internal/common"
 	"gitlab.com/thorchain/bepswap/chain-service/internal/models"
 )
 
 type StakesStore interface {
 	Create(record models.EventStake) error
+	GetStakerAddresses() []common.Address
 }
 
 type stakesStore struct {
@@ -49,6 +54,66 @@ func (s *stakesStore) Create(record models.EventStake) error {
 	if err != nil {
 		return errors.Wrap(err, "Failed to prepareNamed query for StakeRecord")
 	}
-
 	return nil
+}
+
+func (s *stakesStore) GetStakerAddresses() []common.Address {
+
+	query := fmt.Sprintf(`
+		SELECT
+			time_bucket('1 day', time) as bucket,
+			from_address,
+			SUM(stake_total-unstake_total)
+		FROM
+			(SELECT
+				 txs.from_address,
+				 txs.time,
+				 CASE
+					 WHEN SUM(stakes.units) IS NOT NULL THEN SUM(stakes.units)
+					 ELSE 0
+					 END stake_total,
+				 CASE
+					 WHEN SUM(unstakes.units) IS NOT NULL THEN SUM(unstakes.units)
+					 ELSE 0
+					 END unstake_total
+			 FROM
+				 txs
+					 INNER JOIN
+				 stakes
+				 ON txs.event_id = stakes.event_id
+					 LEFT JOIN
+				 unstakes
+				 ON txs.event_id = unstakes.event_id
+			 GROUP BY
+				 txs.from_address, txs.time) x
+		GROUP BY
+			bucket, from_address;	
+	`)
+
+	rows, err := s.db.Queryx(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type results struct {
+		Bucket time.Time
+		From_address string
+		Sum int64
+	}
+
+	var addresses []common.Address
+	for rows.Next() {
+		var result results
+		err = rows.StructScan(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		addr, err := common.NewAddress(result.From_address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		addresses = append(addresses, addr)
+	}
+	return addresses
 }
