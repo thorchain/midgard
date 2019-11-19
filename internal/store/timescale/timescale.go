74a10f2376ec17cd99ecc2f1f0e86aec37bd7a18
Loki Laufeyson <l031@protonmail.com>
2019-11-19 19:05:59 +1100

    WIP for putting in a testsuite.

--- internal/store/timescale/timescale.go
+++ internal/store/timescale/timescale.go
@@ -2,10 +2,13 @@ package timescale
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog"
+	migrate "github.com/rubenv/sql-migrate"
 
 	"gitlab.com/thorchain/bepswap/chain-service/internal/config"
 )
@@ -16,7 +19,7 @@ type Store struct {
 	db       *sqlx.DB
 }
 
-func NewStore(cfg config.TimeScaleConfiguration) *Store {
+func NewStoreConnection(cfg config.TimeScaleConfiguration) *Store {
 	connStr := fmt.Sprintf("user=%s dbname=%s sslmode=%v password=%v", cfg.UserName, cfg.Database, cfg.Sslmode, cfg.Password)
 	db := sqlx.MustConnect("postgres", connStr)
 	return &Store{
@@ -24,3 +27,61 @@ func NewStore(cfg config.TimeScaleConfiguration) *Store {
 		db:       db,
 	}
 }
+
+func NewStore(cfg config.TimeScaleConfiguration) (*Store, error) {
+	connStr := fmt.Sprintf("user=%s sslmode=%v password=%v", cfg.UserName, cfg.Sslmode, cfg.Password)
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		return &Store{}, err
+	}
+	return &Store{
+		cfg:      cfg,
+		db:       db,
+	}, nil
+}
+
+// ------------------------------------------------------------------------------------------------------
+// Used for testing
+
+func (s *Store) Open() (*Store, error) {
+	connStr := fmt.Sprintf("user=%s dbname=%s sslmode=%v password=%v", s.cfg.UserName, s.cfg.Database, s.cfg.Sslmode, s.cfg.Password)
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		return &Store{}, err
+	}
+	return &Store{
+		cfg:      s.cfg,
+		db:       db,
+	}, nil
+}
+
+func (s *Store) CreateDatabase()  {
+	query := fmt.Sprintf(`CREATE DATABASE %v;`, s.cfg.Database)
+	s.db.MustExec(query)
+}
+
+func (s *Store) RunMigrations() {
+	migrations := &migrate.FileMigrationSource{
+		Dir: "../../../db/migrations/",
+	}
+
+	n, err := migrate.Exec(s.db.DB, "postgres", migrations, migrate.Up)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("Applied %d migrations\n", n)
+}
+
+func (s *Store) DropDatabase() {
+	// if err := s.db.Close(); err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	query := fmt.Sprintf(`DROP DATABASE %v;`, s.cfg.Database)
+	res, err := s.db.Exec(query)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	spew.Dump(res)
+}
+// ------------------------------------------------------------------------------------------------------
