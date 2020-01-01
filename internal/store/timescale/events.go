9714becbe66a6ac0c04df2b58531a2b9c97bb18a
Loki Laufeyson <l031@protonmail.com>
2020-01-02 10:51:57 +1100

    * Fixed null return error

--- internal/store/timescale/events.go
+++ internal/store/timescale/events.go
@@ -1,20 +1,18 @@
 package timescale
 
 import (
+  "database/sql"
   "fmt"
-
-  "github.com/pkg/errors"
 
   "gitlab.com/thorchain/midgard/internal/models"
 )
 
 func (s *Client) GetMaxID() (int64, error) {
 	query := fmt.Sprintf("SELECT MAX(id) FROM %s", models.ModelEventsTable)
-	var maxId int64
+	var maxId sql.NullInt64
 	err := s.db.Get(&maxId, query)
 	if err != nil {
-		return 0, errors.Wrap(err, "maxID query return null or failed")
+		return 0, err
 	}
-	return maxId, nil
+	return maxId.Int64, nil
 }
-
