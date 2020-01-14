55c730046e6e213860d1553b5be5cad10ccdd645
Loki Laufeyson <l031@protonmail.com>
2020-01-14 15:49:54 +1100

    * Some reason this one didnt get go fmt ./... the first time...

--- tools/mockServer/mockServer.go
+++ tools/mockServer/mockServer.go
@@ -10,28 +10,27 @@ import (
 )
 
 func main() {
-  addr := ":8081"
-  router := mux.NewRouter()
+	addr := ":8081"
+	router := mux.NewRouter()
 
-  // router.HandleFunc("/", welcome).Methods("GET")
-  router.HandleFunc("/genesis", genesisMockedEndpoint).Methods("GET")
-  router.HandleFunc("/thorchain/events/{id}", eventsMockedEndpoint).Methods("GET")
-  router.HandleFunc("/thorchain/events/tx/{id}", eventsTxMockedEndpoint).Methods("GET")
-  router.HandleFunc("/thorchain/pool_addresses", pool_addresses).Methods("GET")
+	// router.HandleFunc("/", welcome).Methods("GET")
+	router.HandleFunc("/genesis", genesisMockedEndpoint).Methods("GET")
+	router.HandleFunc("/thorchain/events/{id}", eventsMockedEndpoint).Methods("GET")
+	router.HandleFunc("/thorchain/events/tx/{id}", eventsTxMockedEndpoint).Methods("GET")
+	router.HandleFunc("/thorchain/pool_addresses", pool_addresses).Methods("GET")
 
+	// used to debug incorrect dynamically generated requests
+	router.PathPrefix("/").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Request: ", request.URL)
+	})
+	// setup server
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
 
-  // used to debug incorrect dynamically generated requests
-  router.PathPrefix("/").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-    log.Println("Request: ", request.URL)
-  })
-  // setup server
-  srv := &http.Server{
-    Addr:              addr,
-    Handler:           router,
-  }
-
-  fmt.Println("Running mocked endpoints: ", addr)
-  log.Fatal(srv.ListenAndServe())
+	fmt.Println("Running mocked endpoints: ", addr)
+	log.Fatal(srv.ListenAndServe())
 }
 
 func eventsMockedEndpoint(writer http.ResponseWriter, request *http.Request) {
@@ -80,16 +79,15 @@ func pool_addresses(writer http.ResponseWriter, request *http.Request) {
 }
 
 func eventsTxMockedEndpoint(writer http.ResponseWriter, request *http.Request) {
-  log.Println("eventsTxMockedEndpoint Hit!")
-  //vars := mux.Vars(request)
-  //id := vars["id"]
+	log.Println("eventsTxMockedEndpoint Hit!")
+	//vars := mux.Vars(request)
+	//id := vars["id"]
 
-  content, err := ioutil.ReadFile("./thorchain/events/tx/tx.json")
-  if err != nil {
-   log.Fatal(err)
-  }
+	content, err := ioutil.ReadFile("./thorchain/events/tx/tx.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  writer.Header().Set("Content-Type", "application/json")
-  fmt.Fprintf(writer, string(content))
+	writer.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(writer, string(content))
 }
-
