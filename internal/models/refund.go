bff8b55016dcc0a7d91379b4e1bc483d5d53b31d
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-03-12 19:23:22 +0330

    Update fee for refund event

--- internal/models/refund.go
+++ internal/models/refund.go
@@ -3,7 +3,6 @@ package models
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"gitlab.com/thorchain/midgard/internal/clients/thorChain/types"
-	"gitlab.com/thorchain/midgard/internal/common"
 )
 
 type EventRefund struct {
@@ -13,18 +12,6 @@ type EventRefund struct {
 }
 
 func NewRefundEvent(refund types.EventRefund, event types.Event) EventRefund {
-	assetTxCnt := 0
-	for _, out := range event.OutTxs {
-		if !common.IsRune(out.Coins[0].Asset.Ticker) {
-			assetTxCnt++
-		}
-		for _, in := range event.InTx.Coins {
-			if out.Coins[0].Asset.Equals(in.Asset) {
-				event.Fee.Coins = append(event.Fee.Coins, common.NewCoin(in.Asset, in.Amount-out.Coins[0].Amount))
-			}
-		}
-	}
-	event.Fee.PoolDeduct = common.TransactionFee * int64(assetTxCnt)
 	return EventRefund{
 		Code:   refund.Code,
 		Reason: refund.Reason,
--- internal/store/timescale/refund.go
+++ internal/store/timescale/refund.go
@@ -2,19 +2,11 @@ package timescale
 
 import (
 	"github.com/pkg/errors"
-	"gitlab.com/thorchain/midgard/internal/common"
 	"gitlab.com/thorchain/midgard/internal/models"
 )
 
 func (s *Client) CreateRefundRecord(record models.EventRefund) error {
 	pool := record.Fee.Asset()
-	for _, tx := range record.Event.OutTxs {
-		for _, coin := range tx.Coins {
-			if !common.IsRune(coin.Asset.Ticker) {
-				pool = coin.Asset
-			}
-		}
-	}
 	if pool.IsEmpty() {
 		return nil
 	}
@@ -23,7 +15,7 @@ func (s *Client) CreateRefundRecord(record models.EventRefund) error {
 		return errors.Wrap(err, "Failed to get rune depth")
 	}
 	if uint64(record.Fee.PoolDeduct) > runeDepth {
-		record.Fee.PoolDeduct = 0
+		record.Fee.PoolDeduct = int64(runeDepth)
 	}
 	err = s.CreateEventRecord(record.Event)
 	if err != nil {
