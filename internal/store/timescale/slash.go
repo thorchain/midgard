5e166a5fb3168205ec90ddabbbb9fd41f798dfe3
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-03-22 15:05:19 +0430

    Update pool calculation

--- internal/store/timescale/slash.go
+++ internal/store/timescale/slash.go
@@ -2,6 +2,7 @@ package timescale
 
 import (
 	"fmt"
+	"gitlab.com/thorchain/midgard/internal/common"
 
 	"github.com/pkg/errors"
 
@@ -15,23 +16,34 @@ func (s *Client) CreateSlashRecord(record models.EventSlash) error {
 	if err != nil {
 		return errors.Wrap(err, "Failed to create event record")
 	}
-
+	var runeAmt int64
+	var assetAmt int64
 	query := fmt.Sprintf(`
 		INSERT INTO %v (
 			time,
 			event_id,
 			pool,
+			assetAmt,
 			runeAmt,
 			from_address
-		)  VALUES ( $1, $2, $3, $4, $5 ) RETURNING event_id`, models.ModelStakesTable)
+		)  VALUES ( $1, $2, $3, $4, $5, $6 ) RETURNING event_id`, models.ModelStakesTable)
 
 	for _, slash := range record.SlashAmount {
+
+		if common.IsRune(slash.Pool.Ticker) {
+			runeAmt = slash.Amount
+			assetAmt = 0
+		} else {
+			runeAmt = 0
+			assetAmt = slash.Amount
+		}
 		_, err := s.db.Exec(query,
 			record.Event.Time,
 			record.Event.ID,
 			slash.Pool.String(),
-			slash.Amount,
-			addEventAddress,
+			assetAmt,
+			runeAmt,
+			slashEventAddress,
 		)
 		if err != nil {
 			s.logger.Error().Err(err).Msg("failed to prepareNamed query for EventRecord")
