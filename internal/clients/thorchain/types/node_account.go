4d2a8b32c7f22c56d4fdf57032fedde31a413ad0
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-04-04 02:00:51 +0430

    Remove unused feilds

--- internal/clients/thorchain/types/node_account.go
+++ internal/clients/thorchain/types/node_account.go
@@ -3,24 +3,12 @@ package types
 import (
 	"encoding/json"
 	"fmt"
-	"gitlab.com/thorchain/midgard/internal/common"
 	"strings"
 )
 
 type NodeAccount struct {
-	NodeAddress         common.Address `json:"node_address"`
-	Status              NodeStatus     `json:"status"`
-	ValidatorConsPubKey string         `json:"validator_cons_pub_key"`
-	Bond                uint64         `json:"bond,string"`
-	ActiveBlockHeight   int64          `json:"active_block_height,string"`
-	BondAddress         common.Address `json:"bond_address"`
-	SlashPoints         int64          `json:"slash_points,string"`
-	StatusSince         int64          `json:"status_since,string"`
-	ObserverActive      bool           `json:"observer_active"`
-	SignerActive        bool           `json:"signer_active"`
-	RequestedToLeave    bool           `json:"requested_to_leave"`
-	LeaveHeight         int64          `json:"leave_height,string"`
-	Version             string         `json:"version"`
+	Status NodeStatus `json:"status"`
+	Bond   uint64     `json:"bond,string"`
 }
 
 type NodeStatus uint8
--- internal/clients/thorchain/types/vault.go
+++ internal/clients/thorchain/types/vault.go
@@ -1,16 +1,6 @@
 package types
 
-import (
-	"gitlab.com/thorchain/midgard/internal/common"
-)
-
 type VaultType string
-
-const (
-	UnknownVault   VaultType = "unknown"
-	AsgardVault    VaultType = "asgard"
-	YggdrasilVault VaultType = "yggdrasil"
-)
 
 type VaultStatus string
 
@@ -21,21 +11,10 @@ const (
 )
 
 type Vault struct {
-	BlockHeight           int64        `json:"block_height,string"`
-	PubKey                string       `json:"pub_key"`
-	Coins                 common.Coins `json:"coins"`
-	Type                  VaultType    `json:"type"`
-	Status                VaultStatus  `json:"status"`
-	StatusSince           int64        `json:"status_since,string"`
-	Membership            []string     `json:"membership"`
-	InboundTxCount        int64        `json:"inbound_tx_count,string"`
-	OutboundTxCount       int64        `json:"outbound_tx_count,string"`
-	PendingTxBlockHeights []int64      `json:"pending_tx_heights"`
+	BlockHeight int64       `json:"block_height,string"`
+	Status      VaultStatus `json:"status"`
 }
 
 type VaultData struct {
-	BondRewardRune uint64       `json:"bond_reward_rune,string"`
-	TotalBondUnits uint64       `json:"total_bond_units,string"`
-	TotalReserve   uint64       `json:"total_reserve,string"`
-	Gas            common.Coins `json:"gas"`
+	TotalReserve uint64 `json:"total_reserve,string"`
 }
