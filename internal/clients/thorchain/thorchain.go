e0c14a82be10b99dc7880099685f7c10ada74ddf
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-04-20 01:16:12 +0430

    Update processEvents

--- internal/clients/thorchain/thorchain.go
+++ internal/clients/thorchain/thorchain.go
@@ -168,7 +168,7 @@ func (sc *Scanner) processEvents(id int64) (int64, int, error) {
 	for _, evt := range events {
 		maxID = evt.ID
 		sc.logger.Info().Int64("maxID", maxID).Msg("new maxID")
-		if evt.OutTxs == nil {
+		if evt.HasOutboundTx() && evt.OutTxs == nil {
 			outTx, err := sc.client.GetOutTx(evt)
 			if err != nil {
 				sc.logger.Err(err).Msg("GetOutTx failed")
