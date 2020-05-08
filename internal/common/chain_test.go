37b0f26d3a52eb8524b98fdb99a9b3ae9fcc3772
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-05-08 22:36:30 +0430

    Update chain_test.go

--- internal/common/chain_test.go
+++ internal/common/chain_test.go
@@ -9,25 +9,19 @@ type ChainSuite struct{}
 var _ = Suite(&ChainSuite{})
 
 func (s ChainSuite) TestChain(c *C) {
-	bnbChain, err := NewChain("bnb")
+	chain, err := NewChain("bnb")
 	c.Assert(err, IsNil)
-	c.Check(bnbChain.Equals(BNBChain), Equals, true)
-	c.Check(IsBNBChain(bnbChain), Equals, true)
-	c.Check(bnbChain.IsEmpty(), Equals, false)
-	c.Check(bnbChain.String(), Equals, "BNB")
+	c.Check(chain.Equals(BNBChain), Equals, true)
+	c.Check(IsBNBChain(chain), Equals, true)
+	c.Check(chain.IsEmpty(), Equals, false)
+	c.Check(chain.String(), Equals, "BNB")
 
 	_, err = NewChain("B") // too short
 	c.Assert(err, NotNil)
-	_, err = NewChain("BOGUS") // too long
+	_, err = NewChain("LONGCHAIN01") // too long
 	c.Assert(err, NotNil)
-}
 
-func (s ChainSuite) TestChainLength(c *C) {
-	chain, err := NewChain("CH")
-	c.Assert(err.Error(), Equals, "Chain Error: Not enough characters")
 	chain, err = NewChain("THOR")
 	c.Assert(err, IsNil)
 	c.Assert(chain, DeepEquals, THORChain)
-	chain, err = NewChain("LONGCHAIN01")
-	c.Assert(err.Error(), Equals, "Chain Error: Too many characters")
 }
