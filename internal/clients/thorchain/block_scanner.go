c31303f4766f0c1bb6793901d0cdbb5ce3c31d96
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-05-13 17:02:15 +0430

    Add fastSync

--- internal/clients/thorchain/block_scanner.go
+++ internal/clients/thorchain/block_scanner.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/pkg/errors"
@@ -18,7 +19,7 @@ import (
 // Callback represents methods required by Scanner to notify events.
 type Callback interface {
 	NewBlock(height int64, t time.Time, begin, end []abcitypes.Event)
-	NewTx(height int64, index uint32, events []abcitypes.Event)
+	NewTx(height int64, events []abcitypes.Event)
 }
 
 // BlockScanner is a kind of scanner that will fetch events through scanning blocks.
@@ -31,6 +32,7 @@ type BlockScanner struct {
 	stopChan chan struct{}
 	wg       sync.WaitGroup
 	height   int64
+	synced   int64
 	logger   zerolog.Logger
 }
 
@@ -49,6 +51,16 @@ func NewBlockScanner(addr string, interval time.Duration, callback Callback) (*B
 		logger:   log.With().Str("module", "block_scanner").Logger(),
 	}
 	return sc, nil
+}
+
+// SetHeight sets the height that scanner will start scanning from.
+func (sc *BlockScanner) SetHeight(height int64) error {
+	if sc.client.IsRunning() {
+		return errors.New("scanner in running")
+	}
+
+	sc.height = height
+	return nil
 }
 
 // Start will start the scanner.
@@ -74,7 +86,7 @@ func (sc *BlockScanner) Start(height int64) error {
 func (sc *BlockScanner) spawnBlockReader() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
-	query := fmt.Sprintf("tm.event = '%s'", types.EventNewBlock)
+	query := fmt.Sprintf("tm.event = '%s'", types.EventNewBlockHeader)
 	blocks, err := sc.client.Subscribe(ctx, "midgard", query)
 	if err != nil {
 		return errors.Wrapf(err, "failed to subscribe to event '%'", types.EventNewBlock)
@@ -89,14 +101,80 @@ func (sc *BlockScanner) blockReader(events <-chan coretypes.ResultEvent) {
 	defer sc.wg.Done()
 
 	for {
+		if !sc.getSynced() {
+			err := sc.fastSync()
+			if err != nil {
+				sc.logger.Error().Err(err).Msg("failed to fast sync")
+			}
+		}
+
 		select {
 		case e := <-events:
-			block := e.Data.(types.EventDataNewBlock)
-			sc.callback.NewBlock(block.Block.Height, block.Block.Time, block.ResultBeginBlock.Events, block.ResultEndBlock.Events)
+			block := e.Data.(types.EventDataNewBlockHeader)
+			height := block.Header.Height
+			switch {
+			case height == sc.height+1:
+				sc.callback.NewBlock(height, block.Header.Time, block.ResultBeginBlock.Events, block.ResultEndBlock.Events)
+				sc.incrementHeight()
+			case height > sc.height+1:
+				sc.setSynced(false)
+			}
 		case <-sc.stopChan:
 			return
 		}
 	}
+}
+
+func (sc *BlockScanner) getSynced() bool {
+	return atomic.LoadInt64(&sc.synced) == 1
+}
+
+func (sc *BlockScanner) setSynced(synced bool) {
+	var value int64 = 0
+	if synced {
+		value = 1
+	}
+	atomic.StoreInt64(&sc.synced, value)
+}
+
+func (sc *BlockScanner) fastSync() error {
+	err := sc.client.Stop()
+	if err != nil {
+		return errors.Wrap(err, "failed to stop websocket routine")
+	}
+
+	for {
+		height := sc.getHeight()
+		nextHeight := height + 1
+		info, err := sc.client.BlockchainInfo(nextHeight, nextHeight)
+		if err != nil {
+			return errors.Wrap(err, "could not get blockchain info")
+		}
+		if info.LastHeight == height {
+			return nil
+		}
+
+		block, err := sc.client.BlockResults(&height)
+		if err != nil {
+			return errors.Wrapf(err, "could not get results of block %d", height)
+		}
+		for _, tx := range block.TxsResults {
+			sc.callback.NewTx(height, tx.Events)
+		}
+		t := info.BlockMetas[0].Header.Time
+		sc.callback.NewBlock(height, t, block.BeginBlockEvents, block.EndBlockEvents)
+
+		sc.incrementHeight()
+	}
+}
+
+func (sc *BlockScanner) getHeight() int64 {
+	return atomic.LoadInt64(&sc.height)
+}
+
+func (sc *BlockScanner) incrementHeight() {
+	newHeight := atomic.AddInt64(&sc.height, 1)
+	sc.logger.Info().Int64("height", newHeight).Msg("new block scanned")
 }
 
 func (sc *BlockScanner) spawnTxReader() error {
@@ -120,7 +198,7 @@ func (sc *BlockScanner) txReader(events <-chan coretypes.ResultEvent) {
 		select {
 		case e := <-events:
 			tx := e.Data.(types.EventDataTx)
-			sc.callback.NewTx(tx.Height, tx.Index, tx.Result.Events)
+			sc.callback.NewTx(tx.Height, tx.Result.Events)
 		case <-sc.stopChan:
 			return
 		}
@@ -129,7 +207,7 @@ func (sc *BlockScanner) txReader(events <-chan coretypes.ResultEvent) {
 
 // Stop will attempt to stop the scanner (blocking until the scanner stops completely).
 func (sc *BlockScanner) Stop() error {
-	err := sc.client.Start()
+	err := sc.client.Stop()
 	if err != nil {
 		return errors.Wrap(err, "failed to stop websocket routine")
 	}
