7bdaa4e38b87fb35a2fdc3811e787f7650aae567
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-05-19 10:51:36 +0430

    Merge branch 'master' into tendermint-event-handler

--- internal/store/timescale/events_test.go
+++ internal/store/timescale/events_test.go
@@ -59,40 +59,6 @@ func (s *TimeScaleSuite) TestGetMaxID(c *C) {
 }
 
 func (s *TimeScaleSuite) TestGetEventsByTxID(c *C) {
-	evts, err := s.Store.GetEventsByTxID(stakeBnbEvent0.InTx.ID)
-	c.Assert(err, Equals, nil)
-	c.Assert(len(evts), Equals, 0)
-
-	err = s.Store.CreateStakeRecord(stakeBnbEvent0)
-	evts, err = s.Store.GetEventsByTxID(stakeBnbEvent0.InTx.ID)
-	c.Assert(err, Equals, nil)
-	c.Assert(len(evts), Equals, 1)
-	c.Assert(evts[0].ID, Equals, stakeBnbEvent0.ID)
-}
-
-func (s *TimeScaleSuite) TestGetEventsByTxID(c *C) {
-	err := s.Store.CreateEventRecord(stakeBnbEvent0.Event)
-	c.Assert(err, IsNil)
-	event, err := s.Store.GetEventsByTxID(stakeBnbEvent0.InTx.ID)
-	c.Assert(err, IsNil)
-	c.Assert(len(event), Equals, 1)
-	c.Assert(event[0].ID, Equals, stakeBnbEvent0.Event.ID)
-	c.Assert(event[0].Status, Equals, stakeBnbEvent0.Event.Status)
-	c.Assert(event[0].Height, Equals, stakeBnbEvent0.Event.Height)
-	c.Assert(event[0].Type, Equals, stakeBnbEvent0.Event.Type)
-
-	err = s.Store.CreateSwapRecord(swapBuyRune2BoltEvent1)
-	c.Assert(err, IsNil)
-	event, err = s.Store.GetEventsByTxID(swapBuyRune2BoltEvent1.InTx.ID)
-	c.Assert(err, IsNil)
-	c.Assert(len(event), Equals, 1)
-	c.Assert(event[0].ID, Equals, swapBuyRune2BoltEvent1.Event.ID)
-	c.Assert(event[0].Status, Equals, swapBuyRune2BoltEvent1.Event.Status)
-	c.Assert(event[0].Height, Equals, swapBuyRune2BoltEvent1.Event.Height)
-	c.Assert(event[0].Type, Equals, swapBuyRune2BoltEvent1.Event.Type)
-}
-
-func (s *TimeScaleSuite) TestGetEventsByTxID(c *C) {
 	err := s.Store.CreateEventRecord(stakeBnbEvent0.Event)
 	c.Assert(err, IsNil)
 	event, err := s.Store.GetEventsByTxID(stakeBnbEvent0.InTx.ID)
