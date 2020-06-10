c1722a63453005fddc60d87374d9d7db93bd9476
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-06-10 13:43:12 +0430

    Update txs.go

--- internal/store/timescale/txs.go
+++ internal/store/timescale/txs.go
@@ -85,9 +85,10 @@ func (s *Client) buildEventsQuery(address, txID, asset string, eventTypes []stri
 		sb.JoinWithOption(sqlbuilder.LeftJoin, "coins", "txs.tx_hash = coins.tx_hash")
 		sb.Where(sb.Equal("coins.ticker", asset))
 	}
+	doubleSwap := true
+	var types []interface{}
 	if len(eventTypes) > 0 {
-		doubleSwap := false
-		var types []interface{}
+		doubleSwap = false
 		for _, ev := range eventTypes {
 			if ev == "doubleSwap" {
 				doubleSwap = true
@@ -95,22 +96,22 @@ func (s *Client) buildEventsQuery(address, txID, asset string, eventTypes []stri
 				types = append(types, ev)
 			}
 		}
-		query := `SELECT MIN(event_id) 
+	}
+	query := `SELECT MIN(event_id) 
 				FROM   txs 
 				WHERE  direction = 'in' 
 				GROUP  BY tx_hash 
 				HAVING Count(*) = 2 `
-		if doubleSwap {
-			if len(types) > 0 {
-				sb.Where(sb.Or(sb.In("events.type", types...), fmt.Sprintf("txs.event_id in (%s)", query)))
-			} else {
-				sb.Where(fmt.Sprintf("txs.event_id in (%s)", query))
-			}
+	if doubleSwap {
+		if len(types) > 0 {
+			sb.Where(sb.Or(sb.In("events.type", types...), fmt.Sprintf("txs.event_id in (%s)", query)))
 		} else {
-			sb.Where(sb.In("events.type", types...))
-			sb.Where(fmt.Sprintf("txs.event_id not in (%s)", query))
-			sb.Where(" txs.direction = 'in'")
+			sb.Where(fmt.Sprintf("txs.event_id in (%s)", query))
 		}
+	} else {
+		sb.Where(sb.In("events.type", types...))
+		sb.Where(fmt.Sprintf("txs.event_id not in (%s)", query))
+		sb.Where(" txs.direction = 'in'")
 	}
 	return sb.Build()
 }
