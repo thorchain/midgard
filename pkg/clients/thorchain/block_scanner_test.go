a3bb612219786907b58e2606c390c4e5ab944ae7
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-06-12 18:05:20 +0430

    Add unit test for resume scanning

--- pkg/clients/thorchain/block_scanner_test.go
+++ pkg/clients/thorchain/block_scanner_test.go
@@ -230,6 +230,282 @@ func (s *BlockScannerSuite) TestScanningRestart(c *C) {
 	c.Assert(err, NotNil)
 }
 
+func (s *BlockScannerSuite) TestScanningResume(c *C) {
+	now := time.Now()
+	client := &TestTendermint{
+		metas: []*types.BlockMeta{
+			{
+				Header: types.Header{
+					Height: 1,
+					Time:   now,
+				},
+			},
+			{
+				Header: types.Header{
+					Height: 2,
+					Time:   now.Add(time.Second * 3),
+				},
+			},
+		},
+		results: []*coretypes.ResultBlockResults{
+			{
+				Height: 1,
+				TxsResults: []*abcitypes.ResponseDeliverTx{
+					{
+						Events: []abcitypes.Event{
+							{
+								Type: "deliver_tx_event_1",
+								Attributes: []kv.Pair{
+									{
+										Key:   []byte("key1"),
+										Value: []byte("value1"),
+									},
+								},
+							},
+						},
+					},
+				},
+				BeginBlockEvents: []abcitypes.Event{
+					{
+						Type: "begin_event_1",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key2"),
+								Value: []byte("value2"),
+							},
+						},
+					},
+					{
+						Type: "begin_event_2",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key3"),
+								Value: []byte("value3"),
+							},
+						},
+					},
+				},
+				EndBlockEvents: []abcitypes.Event{
+					{
+						Type: "end_event_1",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key4"),
+								Value: []byte("value4"),
+							},
+						},
+					},
+				},
+			},
+			{
+				Height: 2,
+				TxsResults: []*abcitypes.ResponseDeliverTx{
+					{
+						Events: []abcitypes.Event{
+							{
+								Type: "deliver_tx_event_2",
+								Attributes: []kv.Pair{
+									{
+										Key:   []byte("key5"),
+										Value: []byte("value5"),
+									},
+								},
+							},
+							{
+								Type: "deliver_tx_event_3",
+								Attributes: []kv.Pair{
+									{
+										Key:   []byte("key6"),
+										Value: []byte("value6"),
+									},
+								},
+							},
+						},
+					},
+				},
+				BeginBlockEvents: []abcitypes.Event{},
+				EndBlockEvents: []abcitypes.Event{
+					{
+						Type: "end_event_2",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key7"),
+								Value: []byte("value7"),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	callback := &TestCallback{}
+	bc := NewBlockScanner(client, callback, time.Second*3)
+	err := bc.Start()
+	c.Assert(err, IsNil)
+	time.Sleep(time.Second)
+
+	err = bc.Stop()
+	c.Assert(err, IsNil)
+	c.Assert(bc.GetHeight(), Equals, int64(2))
+
+	client.metas = append(client.metas, &types.BlockMeta{
+		Header: types.Header{
+			Height: 3,
+			Time:   now.Add(time.Second * 6),
+		},
+	})
+	client.results = append(client.results, &coretypes.ResultBlockResults{
+		Height: 3,
+		TxsResults: []*abcitypes.ResponseDeliverTx{
+			{
+				Events: []abcitypes.Event{
+					{
+						Type: "deliver_tx_event_4",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key8"),
+								Value: []byte("value8"),
+							},
+						},
+					},
+					{
+						Type: "deliver_tx_event_5",
+						Attributes: []kv.Pair{
+							{
+								Key:   []byte("key9"),
+								Value: []byte("value9"),
+							},
+						},
+					},
+				},
+			},
+		},
+		BeginBlockEvents: []abcitypes.Event{},
+		EndBlockEvents: []abcitypes.Event{
+			{
+				Type: "end_event_3",
+				Attributes: []kv.Pair{
+					{
+						Key:   []byte("key10"),
+						Value: []byte("value10"),
+					},
+				},
+			},
+		},
+	})
+	err = bc.SetHeight(2)
+	c.Assert(err, IsNil)
+	err = bc.Start()
+	c.Assert(err, IsNil)
+	time.Sleep(time.Second)
+
+	err = bc.Stop()
+	c.Assert(err, IsNil)
+	c.Assert(bc.GetHeight(), Equals, int64(3))
+
+	c.Assert(callback.blocks, DeepEquals, []testBlock{
+		{
+			height:    1,
+			blockTime: now,
+			begin: []Event{
+				{
+					Type: "begin_event_1",
+					Attributes: map[string]string{
+						"key2": "value2",
+					},
+				},
+				{
+					Type: "begin_event_2",
+					Attributes: map[string]string{
+						"key3": "value3",
+					},
+				},
+			},
+			end: []Event{
+				{
+					Type: "end_event_1",
+					Attributes: map[string]string{
+						"key4": "value4",
+					},
+				},
+			},
+		},
+		{
+			height:    2,
+			blockTime: now.Add(time.Second * 3),
+			begin:     []Event{},
+			end: []Event{
+				{
+					Type: "end_event_2",
+					Attributes: map[string]string{
+						"key7": "value7",
+					},
+				},
+			},
+		},
+		{
+			height:    3,
+			blockTime: now.Add(time.Second * 6),
+			begin:     []Event{},
+			end: []Event{
+				{
+					Type: "end_event_3",
+					Attributes: map[string]string{
+						"key10": "value10",
+					},
+				},
+			},
+		},
+	})
+	c.Assert(callback.txs, DeepEquals, []testTx{
+		{
+			height: 1,
+			events: []Event{
+				{
+					Type: "deliver_tx_event_1",
+					Attributes: map[string]string{
+						"key1": "value1",
+					},
+				},
+			},
+		},
+		{
+			height: 2,
+			events: []Event{
+				{
+					Type: "deliver_tx_event_2",
+					Attributes: map[string]string{
+						"key5": "value5",
+					},
+				},
+				{
+					Type: "deliver_tx_event_3",
+					Attributes: map[string]string{
+						"key6": "value6",
+					},
+				},
+			},
+		},
+		{
+			height: 3,
+			events: []Event{
+				{
+					Type: "deliver_tx_event_4",
+					Attributes: map[string]string{
+						"key8": "value8",
+					},
+				},
+				{
+					Type: "deliver_tx_event_5",
+					Attributes: map[string]string{
+						"key9": "value9",
+					},
+				},
+			},
+		},
+	})
+}
+
 func (s *BlockScannerSuite) TestScanningFaultTolerant(c *C) {
 	client := &TestTendermint{
 		err: errors.New("failed to fetch data"),
