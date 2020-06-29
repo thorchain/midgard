d225dfe32d3fa05563c1affaadcb572f3e212720
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-06-29 23:40:36 +0430

    Rename "pool_history" to "pools_history"

--- internal/store/timescale/timescale_test.go
+++ internal/store/timescale/timescale_test.go
@@ -14,7 +14,7 @@ import (
 	"gitlab.com/thorchain/midgard/internal/models"
 )
 
-var tables = []string{"coins", "events", "pool_history", "stakes", "swaps", "txs"}
+var tables = []string{"coins", "events", "pools_history", "stakes", "swaps", "txs"}
 
 func Test(t *testing.T) {
 	TestingT(t)
