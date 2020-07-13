778687aabd3dd06559d2c2033f6c0eee572c85f3
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-07-13 21:50:07 +0430

    Update error

--- internal/store/timescale/pools.go
+++ internal/store/timescale/pools.go
@@ -9,7 +9,7 @@ import (
 	"gitlab.com/thorchain/midgard/internal/models"
 )
 
-const ErrPoolNotFound = string("pool does not exist")
+const ErrPoolNotFound = "pool does not exist"
 
 func (s *Client) GetPool(asset common.Asset) (common.Asset, error) {
 	query := `
