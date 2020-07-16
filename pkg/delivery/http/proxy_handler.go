7895969f6754ca52b632d023ec6479817f5795ea
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-07-17 01:42:31 +0430

    Exclude rate limit from proxy

--- pkg/delivery/http/proxy_handler.go
+++ pkg/delivery/http/proxy_handler.go
@@ -2,23 +2,18 @@ package http
 
 import (
 	"fmt"
-	"github.com/patrickmn/go-cache"
-	"golang.org/x/time/rate"
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+	"github.com/yhat/wsutil"
+	"gitlab.com/thorchain/midgard/internal/config"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"path"
 	"strings"
-	"time"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/pkg/errors"
-	"github.com/yhat/wsutil"
-	"gitlab.com/thorchain/midgard/internal/config"
 )
 
-// ProxyHandler will proxy the request to the specified node
+// ProxyHandler will proxy the request to the specified node.
 type ProxyHandler struct {
 	nodes    map[string]nodeProxy
 	basePath string
@@ -28,49 +23,6 @@ type nodeProxy struct {
 	httpProxy      *httputil.ReverseProxy
 	websocketProxy *wsutil.ReverseProxy
 	websocketPath  string
-}
-
-type RateLimitConfig struct {
-	Skipper middleware.Skipper
-	ips     *cache.Cache
-	Rate    rate.Limit
-	Burst   int
-}
-
-func NewRateLimitMiddleware(r float64, b int) echo.MiddlewareFunc {
-	return RateLimitWithConfig(RateLimitConfig{
-		ips:   cache.New(10*time.Minute, 15*time.Minute),
-		Rate:  rate.Limit(r),
-		Burst: b,
-	})
-}
-
-func (r *RateLimitConfig) GetLimiter(ip string) *rate.Limiter {
-	limiter, exists := r.ips.Get(ip)
-	if !exists {
-		limiter := rate.NewLimiter(r.Rate, r.Burst)
-		r.ips.Set(ip, limiter, cache.DefaultExpiration)
-	}
-	return limiter.(*rate.Limiter)
-}
-
-func RateLimitWithConfig(config RateLimitConfig) echo.MiddlewareFunc {
-	if config.Skipper == nil {
-		config.Skipper = middleware.DefaultSkipper
-	}
-	//var limiter = rate.NewLimiter(rate.Limit(config.Limit), config.Burst)
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			var limiter = config.GetLimiter(c.RealIP())
-			if config.Skipper(c) {
-				return next(c)
-			}
-			if limiter.Allow() == false {
-				return echo.ErrTooManyRequests
-			}
-			return next(c)
-		}
-	}
 }
 
 // NewProxyHandler returns a new ProxyHandler with given params.
---
+++ pkg/delivery/http/rate_limiter.go
@@ -0,0 +1,51 @@
+package http
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/patrickmn/go-cache"
+	"golang.org/x/time/rate"
+	"time"
+)
+
+type RateLimitConfig struct {
+	Skipper middleware.Skipper
+	ips     *cache.Cache
+	Rate    rate.Limit
+	Burst   int
+}
+
+func NewRateLimitMiddleware(r float64, b int) echo.MiddlewareFunc {
+	return RateLimitWithConfig(RateLimitConfig{
+		ips:   cache.New(10*time.Minute, 15*time.Minute),
+		Rate:  rate.Limit(r),
+		Burst: b,
+	})
+}
+
+func (r *RateLimitConfig) GetLimiter(ip string) *rate.Limiter {
+	limiter, exists := r.ips.Get(ip)
+	if !exists {
+		limiter := rate.NewLimiter(r.Rate, r.Burst)
+		r.ips.Set(ip, limiter, cache.DefaultExpiration)
+	}
+	return limiter.(*rate.Limiter)
+}
+
+func RateLimitWithConfig(config RateLimitConfig) echo.MiddlewareFunc {
+	if config.Skipper == nil {
+		config.Skipper = middleware.DefaultSkipper
+	}
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			var limiter = config.GetLimiter(c.RealIP())
+			if config.Skipper(c) {
+				return next(c)
+			}
+			if limiter.Allow() == false {
+				return echo.ErrTooManyRequests
+			}
+			return next(c)
+		}
+	}
+}
