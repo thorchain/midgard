d7376b14b12d3f1946ee53019b0af8a8ec5badd3
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-07-23 23:12:33 +0430

    Add tests for GetPoolAggChanges

--- internal/store/timescale/pools_history_test.go
+++ internal/store/timescale/pools_history_test.go
@@ -5,6 +5,7 @@ import (
 
 	"gitlab.com/thorchain/midgard/internal/common"
 	"gitlab.com/thorchain/midgard/internal/models"
+	"gitlab.com/thorchain/midgard/internal/store"
 	. "gopkg.in/check.v1"
 )
 
@@ -82,4 +83,144 @@ func (s *TimeScaleSuite) TestGetEventPool(c *C) {
 	pool, err = s.Store.GetEventPool(2)
 	c.Assert(err, IsNil)
 	c.Assert(pool.String(), Equals, tomobPool.String())
+}
+
+func (s *TimeScaleSuite) TestGetPoolEventAggChanges(c *C) {
+	today := time.Date(2020, 7, 22, 0, 0, 0, 0, time.UTC)
+	tomorrow := today.Add(time.Hour * 24)
+
+	bnbPool, err := common.NewAsset("BNB.BNB")
+	c.Assert(err, IsNil)
+	change := &models.PoolChange{
+		Time:        today,
+		EventID:     1,
+		EventType:   "stake",
+		Pool:        bnbPool,
+		AssetAmount: 100,
+		RuneAmount:  200,
+		Units:       1000,
+	}
+	err = s.Store.UpdatePoolsHistory(change)
+	c.Assert(err, IsNil)
+	change = &models.PoolChange{
+		Time:        today.Add(time.Hour),
+		EventID:     2,
+		EventType:   "swap",
+		Pool:        bnbPool,
+		AssetAmount: -10,
+		RuneAmount:  +20,
+	}
+	err = s.Store.UpdatePoolsHistory(change)
+	c.Assert(err, IsNil)
+	change = &models.PoolChange{
+		Time:        tomorrow,
+		EventID:     3,
+		EventType:   "unstake",
+		Pool:        bnbPool,
+		AssetAmount: -45,
+		RuneAmount:  -110,
+		Units:       -500,
+	}
+	err = s.Store.UpdatePoolsHistory(change)
+	c.Assert(err, IsNil)
+	change = &models.PoolChange{
+		Time:        tomorrow.Add(time.Hour),
+		EventID:     4,
+		EventType:   "swap",
+		Pool:        bnbPool,
+		AssetAmount: +5,
+		RuneAmount:  -12,
+	}
+	err = s.Store.UpdatePoolsHistory(change)
+	c.Assert(err, IsNil)
+
+	changes, err := s.Store.GetPoolAggChanges(bnbPool, "", false, store.DailyTimeBucket, today, tomorrow)
+	c.Assert(err, IsNil)
+	expected := map[int64]models.PoolAggChanges{
+		tomorrow.Unix(): {
+			PosAssetChanges: +5,
+			NegAssetChanges: -45,
+			PosRuneChanges:  0,
+			NegRuneChanges:  -122,
+			UnitsChanges:    -500,
+		},
+		today.Unix(): {
+			PosAssetChanges: 100,
+			NegAssetChanges: -10,
+			PosRuneChanges:  220,
+			NegRuneChanges:  0,
+			UnitsChanges:    1000,
+		},
+	}
+	for _, ch := range changes {
+		exp := expected[ch.Time.Unix()]
+		c.Assert(ch.PosAssetChanges, Equals, exp.PosAssetChanges)
+		c.Assert(ch.NegAssetChanges, Equals, exp.NegAssetChanges)
+		c.Assert(ch.PosRuneChanges, Equals, exp.PosRuneChanges)
+		c.Assert(ch.NegRuneChanges, Equals, exp.NegRuneChanges)
+		c.Assert(ch.UnitsChanges, Equals, exp.UnitsChanges)
+	}
+
+	changes, err = s.Store.GetPoolAggChanges(bnbPool, "", true, store.DailyTimeBucket, today, tomorrow)
+	c.Assert(err, IsNil)
+	expected = map[int64]models.PoolAggChanges{
+		tomorrow.Unix(): {
+			PosAssetChanges: 105,
+			NegAssetChanges: -55,
+			PosRuneChanges:  220,
+			NegRuneChanges:  -122,
+			UnitsChanges:    500,
+		},
+		today.Unix(): {
+			PosAssetChanges: 100,
+			NegAssetChanges: -10,
+			PosRuneChanges:  220,
+			NegRuneChanges:  0,
+			UnitsChanges:    1000,
+		},
+	}
+	for _, ch := range changes {
+		exp := expected[ch.Time.Unix()]
+		c.Assert(ch.PosAssetChanges, Equals, exp.PosAssetChanges)
+		c.Assert(ch.NegAssetChanges, Equals, exp.NegAssetChanges)
+		c.Assert(ch.PosRuneChanges, Equals, exp.PosRuneChanges)
+		c.Assert(ch.NegRuneChanges, Equals, exp.NegRuneChanges)
+		c.Assert(ch.UnitsChanges, Equals, exp.UnitsChanges)
+	}
+
+	changes, err = s.Store.GetPoolAggChanges(bnbPool, "stake", false, store.DailyTimeBucket, today, tomorrow)
+	c.Assert(err, IsNil)
+	expected = map[int64]models.PoolAggChanges{
+		tomorrow.Unix(): {
+			PosAssetChanges: 0,
+			NegAssetChanges: 0,
+			PosRuneChanges:  0,
+			NegRuneChanges:  0,
+			UnitsChanges:    0,
+		},
+		today.Unix(): {
+			PosAssetChanges: 100,
+			NegAssetChanges: 0,
+			PosRuneChanges:  200,
+			NegRuneChanges:  0,
+			UnitsChanges:    1000,
+		},
+	}
+	for _, ch := range changes {
+		exp := expected[ch.Time.Unix()]
+		c.Assert(ch.PosAssetChanges, Equals, exp.PosAssetChanges)
+		c.Assert(ch.NegAssetChanges, Equals, exp.NegAssetChanges)
+		c.Assert(ch.PosRuneChanges, Equals, exp.PosRuneChanges)
+		c.Assert(ch.NegRuneChanges, Equals, exp.NegRuneChanges)
+		c.Assert(ch.UnitsChanges, Equals, exp.UnitsChanges)
+	}
+
+	changes, err = s.Store.GetPoolAggChanges(bnbPool, "swap", true, store.DailyTimeBucket, tomorrow, tomorrow)
+	c.Assert(err, IsNil)
+	c.Assert(changes, HasLen, 1)
+	c.Assert(changes[0].PosAssetChanges, Equals, int64(5))
+	c.Assert(changes[0].NegAssetChanges, Equals, int64(-10))
+	c.Assert(changes[0].PosRuneChanges, Equals, int64(20))
+	c.Assert(changes[0].NegRuneChanges, Equals, int64(-12))
+	c.Assert(changes[0].UnitsChanges, Equals, int64(0))
 }
