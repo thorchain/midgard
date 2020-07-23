677cabac43ac5da71d77abf2bcf72aa5869f678c
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-07-23 23:12:19 +0430

    GetPoolAggChanges reformation

--- internal/models/pool_change.go
+++ internal/models/pool_change.go
@@ -18,18 +18,13 @@ type PoolChange struct {
 	Status      PoolStatus
 }
 
-// PoolEventAggChanges contains aggregated changes of a specific pool and event
+// PoolAggChanges contains aggregated changes of a specific pool or event
 // during a specific time bucket.
-type PoolEventAggChanges struct {
-	Time            time.Time    `db:"time"`
-	Pool            common.Asset `db:"pool"`
-	EventType       string       `db:"event_type"`
-	PosAssetChanges int64        `db:"pos_asset_changes"`
-	NegAssetChanges int64        `db:"neg_asset_changes"`
-	TotalAssetDepth int64        `db:"total_asset_changes"`
-	PosRuneChanges  int64        `db:"pos_rune_changes"`
-	NegRuneChanges  int64        `db:"neg_rune_changes"`
-	TotalRuneDepth  int64        `db:"total_rune_changes"`
-	UnitsChanges    int64        `db:"units_changes"`
-	TotalUnits      int64        `db:"total_units"`
+type PoolAggChanges struct {
+	Time            time.Time `db:"time"`
+	PosAssetChanges int64     `db:"pos_asset_changes"`
+	NegAssetChanges int64     `db:"neg_asset_changes"`
+	PosRuneChanges  int64     `db:"pos_rune_changes"`
+	NegRuneChanges  int64     `db:"neg_rune_changes"`
+	UnitsChanges    int64     `db:"units_changes"`
 }
