a2ba7b8eb96302f754e32b509df2e2cbeec6029b
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-07-24 16:56:54 +0430

    Update adapter.go

--- pkg/delivery/http/adapter.go
+++ pkg/delivery/http/adapter.go
@@ -10,41 +10,41 @@ import (
 )
 
 type connectHandler struct {
-	next echo.HandlerFunc
-	err  error
-	con  echo.Context
+	next    echo.HandlerFunc
+	err     error
+	context echo.Context
 }
 
 func (h *connectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	resBody := new(bytes.Buffer)
-	mw := io.MultiWriter(h.con.Response().Writer, resBody)
-	writer := &responseCopy{Writer: mw, ResponseWriter: h.con.Response().Writer}
-	h.con.Response().Writer = writer
-	h.err = h.next(h.con)
+	mw := io.MultiWriter(h.context.Response().Writer, resBody)
+	writer := &responseCopy{Writer: mw, ResponseWriter: h.context.Response().Writer}
+	h.context.Response().Writer = writer
+	h.err = h.next(h.context)
 	_, err := w.Write(resBody.Bytes())
 	if h.err == nil {
 		h.err = err
 	}
 }
 
-func New1() (http.Handler, func(h http.Handler) echo.MiddlewareFunc) {
+func createHandlers() (http.Handler, func(h http.Handler) echo.MiddlewareFunc) {
 	nextHandler := new(connectHandler)
-	makeGinHandler := func(h http.Handler) echo.MiddlewareFunc {
+	echoHandler := func(h http.Handler) echo.MiddlewareFunc {
 		return func(next echo.HandlerFunc) echo.HandlerFunc {
 			return func(c echo.Context) (err error) {
 				nextHandler.next = next
-				nextHandler.con = c
+				nextHandler.context = c
 				ctx := context.WithValue(c.Request().Context(), nextHandler, c)
 				h.ServeHTTP(c.Response().Writer, c.Request().WithContext(ctx))
 				return nextHandler.err
 			}
 		}
 	}
-	return nextHandler, makeGinHandler
+	return nextHandler, echoHandler
 }
 
 func Wrap(f func(h http.Handler) http.Handler) echo.MiddlewareFunc {
-	next, adapter := New1()
+	next, adapter := createHandlers()
 	return adapter(f(next))
 }
 
