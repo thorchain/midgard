e41732b805cca45c986fec5913ec151c630e65bc
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-08-12 12:41:59 +0430

    Add new event generators

--- internal/store/rand_event_generator.go
+++ internal/store/rand_event_generator.go
@@ -10,6 +10,7 @@ import (
 )
 
 const assetCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const numCharset = "0123456789"
 
 type RandEventGenerator struct {
 	Pools    []common.Asset
@@ -78,20 +79,60 @@ func (g *RandEventGenerator) generateAddress(count int) []common.Address {
 	return addrs
 }
 
-func (g *RandEventGenerator) generateStakeEvent(count int, asset common.Asset) []thorchain.Event {
+func (g *RandEventGenerator) generateTxId() common.TxID {
+	txID,err:=common.NewTxID(g.randString(assetCharset,64))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return txID
+}
+
+func (g *RandEventGenerator) generateStakeEvent(count int,staker common.Address, asset common.Asset) []thorchain.Event {
 	stakeEvents := make([]thorchain.Event, count)
-	address := g.generateAddress(count)
 	for i := 0; i < count; i++ {
 		stakeEvents[i] = thorchain.Event{
 			Type: "stake",
 			Attributes: map[string]string{
 				"pool":         asset.String(),
 				"stake_units":  "10",
-				"rune_address": address[i].String(),
+				"rune_address": staker.String(),
 				"rune_amount":  "1000",
 				"asset_amount": "1000",
 			},
 		}
 	}
 	return stakeEvents
+}
+
+func (g *RandEventGenerator) generateRewardEvent(count int, asset common.Asset) []thorchain.Event {
+	rewardEvents := make([]thorchain.Event, count)
+	for i := 0; i < count; i++ {
+		rewardEvents[i] = thorchain.Event{
+			Type: "reward",
+			Attributes: map[string]string{
+				"bond_reward":  g.randString(numCharset,6),
+				asset.String(): g.randString(numCharset,4),
+			},
+		}
+	}
+	return rewardEvents
+}
+
+func (g *RandEventGenerator) generateAddEvent(count int,staker common.Address,poolAddress common.Address, asset common.Asset) []thorchain.Event {
+	rewardEvents := make([]thorchain.Event, count)
+	for i := 0; i < count; i++ {
+		rewardEvents[i] = thorchain.Event{
+			Type: "add",
+			Attributes: map[string]string{
+				"pool":  g.randString(numCharset, 6),
+				"id":    g.generateTxId().String(),
+				"chain": asset.Chain.String(),
+				"from":  staker.String(),
+				"to":    poolAddress.String(),
+				"coin":  fmt.Sprintf("1000 %s, 2000 %s", asset.String(), common.RuneAsset().String()),
+				"memo":  fmt.Sprintf("ADD:%s", asset.String()),
+			},
+		}
+	}
+	return rewardEvents
 }
