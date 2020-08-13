38dfcaa573592e3cbf9267b51993d7f7da3c49c2
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-08-13 16:32:41 +0430

    Add benchmark for "GetPoolData"

---
+++ internal/store/timescale/benchmark_test.go
@@ -0,0 +1,54 @@
+package timescale
+
+import (
+	"math/rand"
+
+	"gitlab.com/thorchain/midgard/internal/store"
+	. "gopkg.in/check.v1"
+)
+
+type BenchmarkSuite struct {
+	Store     *Client
+	generator *store.RandEventGenerator
+}
+
+var _ = Suite(&BenchmarkSuite{})
+
+func (s *BenchmarkSuite) SetUpSuite(c *C) {
+	var err error
+	s.Store, err = NewTestStore(c)
+	if err != nil {
+		c.Fatal(err.Error())
+	}
+
+	s.generator = store.NewRandEventGenerator(&store.RandEventGeneratorConfig{
+		Source:      rand.NewSource(1878939228537408224),
+		Pools:       10,
+		Stakers:     20,
+		Swappers:    20,
+		Blocks:      10000,
+		AddEvents:   100,
+		StakeEvents: 100,
+		SwapEvents:  2000,
+	})
+	err = s.generator.GenerateEvents(s.Store)
+	if err != nil {
+		c.Fatal(err.Error())
+	}
+}
+
+func (s *BenchmarkSuite) TearDownSuite(c *C) {
+	err := s.Store.MigrationsDown()
+	if err != nil {
+		c.Fatal(err.Error())
+	}
+}
+
+func (s *BenchmarkSuite) BenchmarkGetPoolData(c *C) {
+	for i := 0; i < c.N; i++ {
+		for _, pool := range s.generator.Pools {
+			_, err := s.Store.GetPoolData(pool)
+			c.Assert(err, NotNil)
+		}
+	}
+}
