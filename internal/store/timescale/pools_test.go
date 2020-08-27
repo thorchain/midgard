5ef5609d73f0d11dfde3eae1375cf8c1a118eb57
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-08-27 16:35:39 +0430

    Update staker count

--- internal/store/timescale/pools_test.go
+++ internal/store/timescale/pools_test.go
@@ -1712,10 +1712,40 @@ func (s *TimeScaleSuite) TestStakersCount(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(stakersCount, Equals, uint64(1))
 
-	// Additional stake
-	err = s.Store.CreateStakeRecord(&stakeTomlEvent1)
+	err = s.Store.CreateStakeRecord(&stakeBnbEvent0)
 	c.Assert(err, IsNil)
 
+	stakersCount, err = s.Store.stakersCount(asset)
+	c.Assert(err, IsNil)
+	c.Assert(stakersCount, Equals, uint64(1))
+
+	// Additional stake
+	err = s.Store.CreateStakeRecord(&stakeBnbEvent2)
+	c.Assert(err, IsNil)
+
+	stakersCount, err = s.Store.stakersCount(asset)
+	c.Assert(err, IsNil)
+	c.Assert(stakersCount, Equals, uint64(2))
+
+	// Unstake
+	err = s.Store.CreateUnStakesRecord(&unstakeBnbEvent1)
+	c.Assert(err, IsNil)
+	err = s.Store.CreateUnStakesRecord(&unstakeBnbEvent1)
+	c.Assert(err, IsNil)
+
+	stakersCount, err = s.Store.stakersCount(asset)
+	c.Assert(err, IsNil)
+	c.Assert(stakersCount, Equals, uint64(1))
+
+	// Stake in another pool
+	err = s.Store.CreateStakeRecord(&stakeTcanEvent3)
+	c.Assert(err, IsNil)
+
+	stakersCount, err = s.Store.stakersCount(asset)
+	c.Assert(err, IsNil)
+	c.Assert(stakersCount, Equals, uint64(1))
+
+	asset, _ = common.NewAsset("BNB.TCAN-014")
 	stakersCount, err = s.Store.stakersCount(asset)
 	c.Assert(err, IsNil)
 	c.Assert(stakersCount, Equals, uint64(1))
