291ca25d28b5f2b3975d289e722045eaef59c093
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2020-09-10 20:03:37 +0430

    Complete the repository

--- pkg/repository/event.go
+++ pkg/repository/event.go
@@ -1,26 +1,34 @@
 package repository
 
 import (
+	"database/sql/driver"
 	"time"
 
+	"github.com/pkg/errors"
 	"gitlab.com/thorchain/midgard/internal/common"
 )
 
-// Block contains the time, height and emitted events.
-type Block struct {
-	Time   time.Time
-	Height int64
-	Events []Event
-}
-
-// Event contains the event type, status and changes.
-// Note that some events might affect more than one pool (e.g. rewards).
+// Event contains all the common and specific features of every event type.
 type Event struct {
-	ID            int64
-	Type          EventType
-	Status        EventStatus
-	StatusChanged bool // This will notify the repository to update the event status of records.
-	Changes       []EventChange
+	Time         time.Time
+	Height       int64
+	ID           int64
+	Type         EventType
+	EventID      int64
+	EventType    EventType
+	EventStatus  EventStatus
+	Pool         common.Asset
+	AssetAmount  int64
+	RuneAmount   int64
+	Units        int64
+	TradeSlip    *float64
+	LiquidityFee *int64
+	PriceTarget  *int64
+	FromAddress  string
+	ToAddress    string
+	TxHash       string
+	TxMemo       string
+	PoolStatus   PoolStatus
 }
 
 // EventType determines the type of parent event and change records.
@@ -54,24 +62,6 @@ const (
 	EventStatusSuccess = "success"
 )
 
-// EventChange contains all the common and specific features of every event type.
-type EventChange struct {
-	ID           int64
-	Type         EventType
-	Pool         common.Asset
-	AssetAmount  int64
-	RuneAmount   int64
-	Units        int64
-	TradeSlip    *float64
-	LiquidityFee *int64
-	PriceTarget  *int64
-	FromAddress  string
-	ToAddress    string
-	TxHash       string
-	TxMemo       string
-	PoolStatus   PoolStatus
-}
-
 // PoolStatus determines the current status of pool.
 // https://gitlab.com/thorchain/thornode/blob/6ff70aa3ab7da1f418fcb6f34840c1f160be7f06/x/thorchain/types/type_pool.go#L14
 type PoolStatus string
@@ -82,3 +72,27 @@ const (
 	PoolStatusBootstrap = "Bootstrap"
 	PoolStatusSuspended = "Suspended"
 )
+
+func (s PoolStatus) String() string {
+	return string(s)
+}
+
+func (s *PoolStatus) Scan(v interface{}) error {
+	if v == nil {
+		*s = ""
+		return nil
+	}
+
+	if str, ok := v.(string); ok {
+		*s = PoolStatus(str)
+		return nil
+	}
+	return errors.Errorf("could not scan type %T as PoolStatus", v)
+}
+
+func (s PoolStatus) Value() (driver.Value, error) {
+	if s == "" {
+		return nil, nil
+	}
+	return s, nil
+}
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"context"
 	"time"
 
 	"gitlab.com/thorchain/midgard/internal/common"
@@ -9,18 +10,30 @@ import (
 
 // Repository represents methods required by Usecase to store/load data from internal data store.
 type Repository interface {
-	NewBlock(block *Block) error
-	GetEventIDByTxHash(hash string) (int64, error)
-	GetTxDetails(address common.Address, txID common.TxID, asset common.Asset, eventTypes []string, offset, limit int64) ([]models.TxDetails, int64, error)
-	GetPool(asset common.Asset, at *time.Time) (*models.PoolBasics, error)
-	GetPools(assetQuery string, status models.PoolStatus, offset, limit int64) ([]models.PoolBasics, error)
-	GetStats(at *time.Time) (*models.StatsData, error)
-	GetUsersCount(from, to *time.Time) (uint64, error)
-	GetStakers() ([]common.Address, error)
-	GetStakerDetails(address common.Address) (*models.StakerAddressDetails, error)
-	GetStakerAssetDetails(address common.Address, asset common.Asset) (models.StakerAddressAndAssetDetails, error)
-	GetTotalVolChanges(interval models.Interval, from, to time.Time) ([]models.TotalVolChanges, error)
-	GetPoolAggChanges(pool common.Asset, inv models.Interval, from, to time.Time) ([]models.PoolAggChanges, error)
+	// BeginTx will prepare a new tx for all the changes of block to database.
+	BeginTx(ctx context.Context) (Tx, error)
+	// GetEventByTxHash returns all the corresponding event records for the given tx hash.
+	GetEventByTxHash(ctx context.Context, hash string) ([]Event, error)
+	GetEvents(ctx context.Context, address common.Address, asset common.Asset, types []EventType) ([]Event, int64, error)
+	GetPool(ctx context.Context, asset common.Asset, at *time.Time) (*models.PoolBasics, error)
+	GetPools(ctx context.Context, assetQuery string, status models.PoolStatus) ([]models.PoolBasics, error)
+	GetStats(ctx context.Context, at *time.Time) (*models.StatsData, error)
+	GetUsersCount(ctx context.Context, from, to *time.Time) (int64, error)
+	GetStakers(ctx context.Context, address common.Address, asset common.Asset) ([]Staker, error)
+	GetTotalVolChanges(ctx context.Context, interval models.Interval, from, to time.Time) ([]models.TotalVolChanges, error)
+	GetPoolAggChanges(ctx context.Context, pool common.Asset, interval models.Interval, from, to time.Time) ([]models.PoolAggChanges, error)
 	GetLatestState() (*LatestState, error)
 	Ping() error
+}
+
+// Tx represents methods required to update the database atomically.
+type Tx interface {
+	NewEvents(changes []Event) error
+	SetEventStatus(id int64, status EventStatus) error
+	NewPool(asset common.Asset) error
+	UpdatePool(pool *models.PoolBasics) error
+	UpdateStats(stats *models.StatsData) error
+	UpsertStaker(staker *Staker) error
+	Commit() error
+	RollBack() error
 }
