d6b10d947f1f5da0fe4cc1d362f04461b9e9d206
Ragnar Lodbrok <ehsan.saradar@gmail.com>
2020-11-11 14:23:15 +0330

    Update stakeUnits sql query

--- internal/store/timescale/stakes.go
+++ internal/store/timescale/stakes.go
@@ -144,8 +144,9 @@ func (s *Client) stakeUnits(address common.Address, asset common.Asset) (uint64,
 			   JOIN events 
 				 ON pools_history.event_id = events.id 
 		WHERE  pools_history.pool = $1 
-			   AND txs.from_address = $2 
-			   AND events.status = 'Success' `
+			   AND ( txs.from_address = $2 
+					  OR txs.to_address = $2 ) 
+			   AND events.status = 'Success'`
 
 	var stakeUnits sql.NullInt64
 	err := s.db.Get(&stakeUnits, query, asset.String(), address)
